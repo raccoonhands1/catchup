f426fe4200a9014a106bbadb0d0f95c90fe12181
Enkhbold Ganbold <53986637+enkhbold470@users.noreply.github.com>
2024-07-20 21:54:14 +0000

    navbar and hero

--- frontend_catchup/src/app/page.tsx
+++ frontend_catchup/src/app/page.tsx
@@ -1,7 +1,8 @@
-
+import Navbar from "@/components/navbar";
 export default function Home() {
   return (
     <>
+    <Navbar/>
       {/* <ResponsiveDebug /> */}
 
       {/* <BouncyCardsFeatures /> */}
