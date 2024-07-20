0c672de2780abc2dd1366084cd0a42e7cef5f295
Enkhbold Ganbold <53986637+enkhbold470@users.noreply.github.com>
2024-07-20 22:00:26 +0000

    Hero and Navigation bar are separated

--- frontend_catchup/src/app/page.tsx
+++ frontend_catchup/src/app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
 export default function Home() {
   return (
     <>
     <Navbar/>
+    <Hero />
       {/* <ResponsiveDebug /> */}
 
       {/* <BouncyCardsFeatures /> */}
