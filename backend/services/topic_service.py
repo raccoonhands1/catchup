753f85bf60143201dc62a09ec8b91ec333bf93b1
Akiko0210 <qea643@gmail.com>
2024-07-21 01:36:32 -0700

    created service functions

--- backend/services/topic_service.py
+++ backend/services/topic_service.py
@@ -1,23 +1,24 @@
 from db import db
+from schemas import Article, Topic
 
 topics_collection = db.topics
 
-def add_topic(topic_data):
-    topics_collection.insert_one(topic_data)
-    return {"message": "Topic added successfully!"}
-
-def get_all_topics():
-    topics = list(topics_collection.find({}, {'_id': 0}))
-    return topics
-
-def get_topic_by_key_value(key, value):
-    topic = topics_collection.find_one({key: value}, {'_id': 0})
-    return topic if topic else {"message": "No topic found!"}
-
-def update_topic(query, update):
-    result = topics_collection.update_many(query, {'$set': update})
-    return {"message": f"{result.modified_count} topics updated!"}
-
-def delete_topic(query):
-    result = topics_collection.delete_many(query)
-    return {"message": f"{result.deleted_count} topics deleted!"}
+def add_topic(data) -> str:
+    articles_data = data.get('articles', [])
+    articles = [Article(
+        title=article['title'],
+        authors=article['authors'],
+        abstract=article['abstract'],
+        topics=article['topics'],
+        url=article['url'],
+        date=article['date']
+    ) for article in articles_data]
+    
+    topic = Topic(
+        name=data['name'],
+        subscribed_users=data['subscribed_users'],
+        articles=articles
+    )
+    
+    topic_id = topics_collection.insert_one(topic.to_dict()).inserted_id
+    return str(topic_id)
--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -1,40 +1,17 @@
 from db import db
-from schemas.user_schema import user_schema, is_objectid
-from cerberus import Validator
-from bson import ObjectId
+from schemas import User
 
 users_collection = db.users
-validator = Validator(user_schema)
-validator.add_check(is_objectid)
 
-def add_user(user_data):
-    if validator.validate(user_data):
-        user_data['subscribed_topics'] = [ObjectId(topic) for topic in user_data['subscribed_topics']]
-        users_collection.insert_one(user_data)
-        return {"message": "User added successfully!"}
-    else:
-        return {"message": validator.errors}
+def add_user(data):
+    user = User(persona=data['persona'], username=data['username'], email=data['email'])
+    user_id = users_collection.insert_one(user.to_dict()).inserted_id
+    return str(user_id)
 
 def get_all_users():
-    users = list(users_collection.find({}, {'_id': 0}))
-    return users
-
-def get_user_by_key_value(key, value):
-    if key == "_id" and ObjectId.is_valid(value):
-        value = ObjectId(value)
-    user = users_collection.find_one({key: value}, {'_id': 0})
-    return user if user else {"message": "No user found!"}
-
-def update_user(query, update):
-    if '_id' in query and ObjectId.is_valid(query['_id']):
-        query['_id'] = ObjectId(query['_id'])
-    if 'subscribed_topics' in update:
-        update['subscribed_topics'] = [ObjectId(topic) for topic in update['subscribed_topics']]
-    result = users_collection.update_many(query, {'$set': update})
-    return {"message": f"{result.modified_count} users updated!"}
-
-def delete_user(query):
-    if '_id' in query and ObjectId.is_valid(query['_id']):
-        query['_id'] = ObjectId(query['_id'])
-    result = users_collection.delete_many(query)
-    return {"message": f"{result.deleted_count} users deleted!"}
+    users = list(users_collection.find())
+    output = []
+    for user in users:
+        user['_id'] = str(user['_id'])  # Convert ObjectId to string
+        output.append(user)
+    return output
