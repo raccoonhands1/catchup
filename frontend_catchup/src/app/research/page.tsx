81818aeb32e298936fea75aad6ea2563660dfb68
Enkhbold Ganbold <53986637+enkhbold470@users.noreply.github.com>
2024-07-21 18:11:13 +0000

    comment section need to col

--- frontend_catchup/src/app/research/page.tsx
+++ frontend_catchup/src/app/research/page.tsx
@@ -18,24 +18,34 @@ import { Progress } from "@/components/ui/progress";
 
 interface ImpactItem {
   num: number;
-  words: string;
+  url: string;
+  title: string;
+  authors: string;
+  time: string;
+  abstract: string;
 }
 const impact: ImpactItem[] = [
-  {
-    num: 10,
-    words: `Oxygen gets you high. In a catastrophic emergency, we're taking giant, panicked breaths.`,
-  },
-  {
-    num: 50,
-    words: `This code introduces a button that, when clicked, increments the value of`,
-  },
-  {
-    num: 90,
-    words: `Add a Button to Trigger the Update: Include a button in the JSX that, when impact.number.`,
-  },
+  // {
+  //   num: 10, // value of impact/ severity
+  //   words: `Oxygen gets you high. In a catastrophic emergency, we're taking giant, panicked breaths.`, //summary
+  // },
+  // {
+  //   num: 50,
+  //   words: `This code introduces a button that, when clicked, increments the value of`,
+  // },
+  // {
+  //   num: 90,
+  //   words: `Add a Button to Trigger the Update: Include a button in the JSX that, when impact.number.`,
+  // },
   {
     num: 40,
-    words: `Calls the function to update impact.number. Add a Button to Trigger the at, when clicked`,
+    url: "https://arxiv.org/abs/2407.12795",
+    title: "How to make a button",
+    authors:
+      "Ethan Mollick, Lilach Mollick, Natalie Bach, LJ Ciccarelli, Ben Przystanski, Daniel Ravipinto",
+    time: "19 July 2024",
+    abstract:
+      "This paper explores the potential of generative AI in creating adaptive educational simulations. By leveraging a system of multiple AI agents, simulations can provide personalized learning experiences, offering students the opportunity to practice skills in scenarios with AI-generated mentors, role-players, and instructor-facing evaluators. We describe a prototype, PitchQuest, a venture capital pitching simulator that showcases the capabilities of AI in delivering instruction, facilitating practice, and providing tailored feedback. The paper discusses the pedagogy behind the simulation, the technology powering it, and the ethical considerations in using AI for education. While acknowledging the limitations and need for rigorous testing, we propose that generative AI can significantly lower the barriers to creating effective, engaging simulations, opening up new possibilities for experiential learning at scale.",
   },
 ];
 
@@ -75,8 +85,7 @@ export default function SidebarDemo() {
   return (
     <div
       className={cn(
-        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-        "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
+        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden" // for your use case, use `h-screen` instead of `h-[60vh]`
       )}
     >
       <Sidebar open={open} setOpen={setOpen}>
@@ -146,18 +155,25 @@ const LogoIcon = () => {
 const Dashboard = () => {
   return (
     <div className="flex flex-1">
-      <div className="p-2 md:p-10 rounded-tl-3xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+      <div className="p-2 md:p-10 rounded-tl-3xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full">
         {/* boxes */}
 
         {impact.map((item, index) => (
           <div key={index} className="flex gap-2">
             <div
               key={index}
-              className="h-[10rem] w-full p-4 rounded-2xl bg-gray-100 inset-1"
+              className="h-[40rem] w-full p-4 rounded-2xl bg-gray-100 inset-1"
             >
-              <TextGenerateEffect words={impact[index].words} />
+              <h1>{impact[index].authors}</h1>
+              <h2>{impact[index].title}</h2>
+              <h3>{impact[index].time}</h3>
+              <TextGenerateEffect words={impact[index].abstract} />
             </div>
-            <div key={index} className="w-full p-4 rounded-2xl bg-gray-100">
+            {/* impact bar*/}
+            <div
+              key={index}
+              className="w-full h-min p-4 rounded-2xl bg-gray-100"
+            >
               {/* <TextGenerateEffect words={words} /> */}
               <div className="flex justify-center w-full pb-2 font-bold">
                 How impactful to your startup?
@@ -166,6 +182,21 @@ const Dashboard = () => {
                 <Progress value={item.num} />
               </div>
             </div>
+            {/* impact bar*/}
+            {/* comment section*/}
+            <div
+              key={index}
+              className="w-full h-min p-4 rounded-2xl bg-gray-100"
+            >
+              {/* <TextGenerateEffect words={words} /> */}
+              <div className="flex justify-center w-full pb-2 font-bold">
+                Comment:
+              </div>
+              <div className="w-full ">
+                <Progress value={item.num} />
+              </div>
+            </div>
+            {/* comment section*/}
           </div>
         ))}
 
