3268d9117359cae55f184663e6a7ab41bcd296ee
Akiko0210 <qea643@gmail.com>
2024-07-21 10:43:14 -0700

    socket

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_socketio import SocketIO
 from routes.user_routes import user_bp
 from routes.topic_routes import topic_bp
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 app.register_blueprint(user_bp)
 app.register_blueprint(topic_bp)
@@ -12,5 +14,5 @@ def home():
     return "Welcome to the Flask MongoDB App!"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    socketio.run(app, debug=True, port=5000)
 
