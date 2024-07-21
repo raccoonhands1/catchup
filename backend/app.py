6f0d2f3e9f9ef3ef6792448aa156ec632183bbf6
Akiko0210 <qea643@gmail.com>
2024-07-21 11:42:15 -0700

    socket subscribe

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room
 from routes.user_routes import user_bp
 from routes.topic_routes import topic_bp
 
@@ -12,6 +12,12 @@ app.register_blueprint(topic_bp)
 @app.route('/')
 def home():
     return "Welcome to the Flask MongoDB App!"
+
+@socketio.on('join')
+def on_join(data):
+    topic_id = data['topic_id']
+    join_room(topic_id)
+    print(f'Client joined room: {topic_id}')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5000)
