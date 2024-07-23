ac7ff4280f706f0b00bf3e012e524474c1c7e66e
Enkhbold Ganbold <enkhbold470@gmail.com>
2024-07-23 04:36:02 -0700

    marketplace update

--- frontend_catchup/src/app/marketplace/page.tsx
+++ frontend_catchup/src/app/marketplace/page.tsx
@@ -1,79 +1,42 @@
 "use client";
-import React, { useState } from "react";
-import { Sidebar, SidebarBody, SidebarLink } from "@/components/sidebar";
-import { IconBook2 } from "@tabler/icons-react";
-import Link from "next/link";
-import { motion } from "framer-motion";
-import { cn } from "@/lib/utils";
-import { Bars3BottomLeftIcon, Bars3Icon } from "@heroicons/react/24/outline";
-import { IconArrowBigLeftLines } from "@tabler/icons-react";
-import { TextGenerateEffect } from "@/components/text-generate-effect";
-import { InputForm } from "@/components/input-form";
-import { Textarea } from "@/components/ui/textarea";
+import React, { useState, useEffect } from "react";
 import articleJson from "@/lib/articles.json";
+import { MultiStepLoader as Loader } from "@/components/ui/multi-step-loader";
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+export default function MultiStepLoaderDemo() {
+  const [loading, setLoading] = useState(true);
+  const [showDashboard, setShowDashboard] = useState(false);
 
-export default function SidebarDemo() {
-  const [open, setOpen] = useState(false);
+  const loop = false; // This might be dynamic in your actual code
+
+  useEffect(() => {
+    if (loading) {
+      const timer = setTimeout(() => {
+        // Action to perform after duration ends
+        setLoading(false);
+        setShowDashboard(true);
+      }, 2000); // Assuming '100' is the duration in milliseconds
+
+      // Cleanup function to clear the timeout
+      return () => clearTimeout(timer);
+    }
+  }, [loading]); // This effect depends on the `loop` state
   return (
-    <div
-      className={cn(
-        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden" // for your use case, use `h-screen` instead of `h-[60vh]`
-      )}
-    >
-      <Sidebar open={open} setOpen={setOpen}>
-        <SidebarBody className="justify-between gap-20 ">
-          <div className="flex flex-col flex-1 overflow-y-auto">
-            {open ? <Logo /> : <LogoIcon />}
-            <div className="mt-8 flex flex-col gap-2">
-              {articleJson.map((tags, idx) => (
-                <SidebarLink
-                  key={idx}
-                  label={articleJson[idx].tags[0]}
-                  className=" rounded-xl p-1 flex"
-                />
-              ))}
-            </div>
-          </div>
-        </SidebarBody>
-      </Sidebar>
-      <Dashboard />
+    <div className="w-full flex items-center justify-center">
+      {/* Core Loader Modal */}
+      <Loader
+        loadingStates={articleJson.map((title) => ({
+          text: title.title,
+        }))}
+        loading={loading}
+        duration={100}
+        loop={false}
+      />
+
+      {showDashboard && <Dashboard />}
     </div>
   );
 }
-const Logo = () => {
-  return (
-    <Link
-      href="#"
-      className="font-normal flex space-x-2 items-center text-sm  py-1 relative z-20"
-    >
-      <IconArrowBigLeftLines stroke={1.5} className="h-6 w-6" />
-      <motion.span
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        className="font-medium text-black dark:text-white whitespace-pre"
-      >
-        Topics
-      </motion.span>
-    </Link>
-  );
-};
-const LogoIcon = () => {
-  return (
-    <Link
-      href="#"
-      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
-    >
-      <Bars3BottomLeftIcon aria-hidden="true" className="h-6 w-6" />
-    </Link>
-  );
-};
 
 // Dummy dashboard component with content
 const Dashboard = () => {
